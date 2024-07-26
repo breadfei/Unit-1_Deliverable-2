5e5a6831fa0bc667dd6f95d736cb261aa8dedc03
brady <brady@DESKTOP-IIRTOLL>
2024-07-25 20:45:15 -0400

    Add project files.

---
+++ Deliverable2/Program.cs
@@ -0,0 +1,43 @@
+﻿Random rnd = new Random();
+int score = 0;
+
+Console.WriteLine("What is your name?");
+string name = Console.ReadLine();
+Console.WriteLine(name + ", would you like to do the Coin Flip Challenge?");
+string answer = Console.ReadLine();
+if (answer == "Yes")
+{
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine("Heads or Tails?");
+        answer = Console.ReadLine();
+        if (rnd.Next(0,2) == 0)
+        {
+            if (answer == "Heads")
+            {
+                Console.WriteLine("Correct!");
+                score++;
+            }else
+            {
+                Console.WriteLine("Wrong!");
+            }
+        }else
+        {
+            if (answer == "Tails")
+            {
+                Console.WriteLine("Correct!");
+                score++;
+            }
+            else
+            {
+                Console.WriteLine("Wrong!");
+            }
+        }
+    }
+
+    Console.WriteLine("Thank you, " + name + ". You got a score of " + score + ".");
+}
+else
+{
+    Console.WriteLine("You're a coward, " + name + ".");
+}
